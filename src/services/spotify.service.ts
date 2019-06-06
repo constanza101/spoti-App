9d5e18b617f4abb960e9a9832a2a5751752ba05f
Constanza Díaz <constanza.diaz@gmail.com>
2019-06-06 14:15:10 +0200

    git

--- src/services/spotify.service.ts
+++ src/services/spotify.service.ts
@@ -39,7 +39,7 @@ export class SpotifyService {
       'Authorization': 'Bearer BQAEXcO4bFf_eG_3ZZxdWT43AHfbzIK0cRIj-coCaoq2UCNYcYfuXrOEGZrTxm_jW7gCwdnBE7aD259ciqU'
     });
 
-//hola
+
     
     return this._http.get(URL, {headers})
       .pipe( map( data => {
